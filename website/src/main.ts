bfaa61d750b049ffa12bc88ee07d2b1f79be24b5
alexsad95 <aleksandrsadchikov.17@gmail.com>
2024-05-04 22:45:47 +0300

    (feat) added prettier, script and logic to main.ts

--- website/src/main.ts
+++ website/src/main.ts
@@ -1,7 +1,50 @@
-import './scss/style.scss';
+import './scss/style.scss'; // for adding project styles
 
-function sumOperation(a: number, b: number) {
-  return a + b; 
+document.addEventListener('DOMContentLoaded', () => {
+  const indeterminateCheckbox = document.getElementById('checkbox-french');
+  const toggleThemeButton = document.getElementById('theme-button');
+  const activeOutlineButton = document.getElementById('active-outline-button');
+  const activeButton = document.getElementById('active-button');
+
+  if (!(indeterminateCheckbox && toggleThemeButton && activeOutlineButton && activeButton)) {
+    console.error(
+      'Not found some elements: ',
+      indeterminateCheckbox,
+      toggleThemeButton,
+      activeOutlineButton,
+      activeButton,
+    );
+    return;
+  }
+
+  // indeterminated checkbox
+  (indeterminateCheckbox as HTMLInputElement).indeterminate = true;
+
+  // add events for buttons
+  const toggleActiveOutliineButtonHandler = () => toggleActiveButton('active-outline-button');
+  const toggleActiveButtonHandler = () => toggleActiveButton('active-button');
+
+  activeOutlineButton.addEventListener('click', toggleActiveOutliineButtonHandler);
+  activeButton.addEventListener('click', toggleActiveButtonHandler);
+  toggleThemeButton.addEventListener('click', toggleTheme);
+});
+
+function toggleActiveButton(elemID: string) {
+  if (!(elemID && typeof elemID === 'string')) {
+    throw new Error('Not a string or empty');
+  }
+
+  const activeButtonElement = document.getElementById(elemID);
+  if (!activeButtonElement) {
+    return;
+  }
+
+  activeButtonElement.classList.toggle('active');
+
+  const isActive = activeButtonElement.innerText === 'active';
+  activeButtonElement.innerText = isActive ? 'not active' : 'active';
 }
 
-console.log('sum result is: ', sumOperation(1,2));
+function toggleTheme() {
+  document.body.classList.toggle('light-theme');
+}
