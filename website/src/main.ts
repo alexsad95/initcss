acb3d1f137b4d694246088a56ba2aeeb5bcea303
alexsad95 <aleksandrsadchikov.17@gmail.com>
2025-05-29 15:58:24 +0300

    remove some sizes, added new styles, refactoring

--- website/src/main.ts
+++ website/src/main.ts
@@ -3,36 +3,25 @@ import tippy from 'tippy.js';
 import { defaultTippyOptions } from './utils/constants';
 import { getCookie, setCookie } from './utils/cookies';
 
-import 'tippy.js/dist/tippy.css';
 import './scss/style.scss';
 
 type Themes = 'dark' | 'light';
 
 // Base page elements
-const indeterminateCheckbox = document.getElementById('checkbox-french');
 const toggleThemeButton = document.getElementById('theme-button');
-const themeChangeButton = document.getElementById('theme-changing');
+// const themeChangeButton = document.getElementById('theme-changing');
 const activeOutlineButton = document.getElementById('active-outline-button');
 const activeButton = document.getElementById('active-button');
-const themeChangingBlock = document.getElementById('theme-changing-block');
+// const themeChangingBlock = document.getElementById('theme-changing-block');
 
 /**
  * Main event handler for initializing UI elements and setting up theme
  * and button actions after page load
  */
 document.addEventListener('DOMContentLoaded', () => {
-  if (
-    !(
-      indeterminateCheckbox &&
-      toggleThemeButton &&
-      activeOutlineButton &&
-      activeButton &&
-      themeChangeButton
-    )
-  ) {
+  if (!(toggleThemeButton && activeOutlineButton && activeButton)) {
     console.error(
       'Not found some elements: ',
-      indeterminateCheckbox,
       toggleThemeButton,
       activeOutlineButton,
       activeButton,
@@ -51,23 +40,23 @@ document.addEventListener('DOMContentLoaded', () => {
   checkThemeForIcons();
 
   // Indeterminated checkbox
-  (indeterminateCheckbox as HTMLInputElement).indeterminate = true;
+  // (indeterminateCheckbox as HTMLInputElement).indeterminate = true;
 
   // Add events for buttons
-  document.addEventListener('click', (event) => closeActivatedThemeChanger(event));
+  // document.addEventListener('click', (event) => closeActivatedThemeChanger(event));
   toggleThemeButton.addEventListener('click', toggleTheme);
   activeOutlineButton.addEventListener('click', () => toggleActiveButton(activeOutlineButton));
   activeButton.addEventListener('click', () => toggleActiveButton(activeButton));
-  themeChangeButton.addEventListener('click', () => activateThemeChanger());
+  // themeChangeButton.addEventListener('click', () => activateThemeChanger());
 
   tippy(toggleThemeButton, {
     ...defaultTippyOptions,
     content: 'Theme switcher',
   });
-  tippy(themeChangeButton, {
-    ...defaultTippyOptions,
-    content: 'Change themes',
-  });
+  // tippy(themeChangeButton, {
+  //   ...defaultTippyOptions,
+  //   content: 'Change themes',
+  // });
 });
 
 /**
@@ -128,32 +117,32 @@ function checkThemeForIcons(): void {
 /**
  * Activates or deactivates theme changing block
  */
-function activateThemeChanger(): void {
-  if (!themeChangingBlock) {
-    console.error('Not found theme changing block');
-    return;
-  }
+// function activateThemeChanger(): void {
+//   if (!themeChangingBlock) {
+//     console.error('Not found theme changing block');
+//     return;
+//   }
 
-  themeChangingBlock.classList.toggle('active');
-}
+//   themeChangingBlock.classList.toggle('active');
+// }
 
 /**
  * Closes theme changing block if it's activated and user clicked outside of it
  */
-function closeActivatedThemeChanger(event: MouseEvent): void {
-  if (!themeChangingBlock) {
-    console.error('Not found theme changing block');
-    return;
-  }
+// function closeActivatedThemeChanger(event: MouseEvent): void {
+//   if (!themeChangingBlock) {
+//     console.error('Not found theme changing block');
+//     return;
+//   }
 
-  const target = event.target as HTMLElement;
+//   const target = event.target as HTMLElement;
 
-  if (
-    target &&
-    !target.closest('#theme-changing') &&
-    !target.closest('#theme-changing-block') &&
-    themeChangingBlock.classList.contains('active')
-  ) {
-    themeChangingBlock.classList.remove('active');
-  }
-}
+//   if (
+//     target &&
+//     !target.closest('#theme-changing') &&
+//     !target.closest('#theme-changing-block') &&
+//     themeChangingBlock.classList.contains('active')
+//   ) {
+//     themeChangingBlock.classList.remove('active');
+//   }
+// }
